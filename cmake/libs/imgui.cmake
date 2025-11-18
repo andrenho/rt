5d2c9841a1d75f84b581e04086b350bfeabf130a
Andre Wagner <andre.nho@gmail.com>
2025-11-18 14:11:35 -0600

    .

---
+++ cmake/libs/imgui.cmake
@@ -0,0 +1,35 @@
+set(OpenGL_GL_PREFERENCE "LEGACY")
+find_package(OpenGL 2 REQUIRED)
+
+FetchContent_Populate(imgui
+        URL https://github.com/ocornut/imgui/archive/docking.zip
+        SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/imgui
+)
+
+FetchContent_Populate(
+        rlimgui
+        GIT_REPOSITORY "https://github.com/raylib-extras/rlImGui.git"
+        GIT_TAG "main"
+        GIT_PROGRESS TRUE
+)
+
+add_library(imgui_raylib STATIC
+        imgui/imgui.cpp
+        imgui/imgui_draw.cpp
+        imgui/imgui_demo.cpp
+        imgui/imgui_tables.cpp
+        imgui/imgui_widgets.cpp
+
+        imgui/backends/imgui_impl_opengl2.cpp
+        rlimgui-src/rlImGui.cpp
+)
+
+target_include_directories(imgui_raylib
+        PUBLIC SYSTEM
+        ${CMAKE_CURRENT_BINARY_DIR}/imgui
+        ${CMAKE_CURRENT_BINARY_DIR}/imgui/backends
+        ${CMAKE_CURRENT_BINARY_DIR}/rlimgui-src
+)
+
+target_link_libraries(imgui_raylib PUBLIC ${OPENGL_LIBRARIES})
+
--- map/test/main.cc
+++ map/test/main.cc
@@ -1,6 +1,50 @@
+#include <raylib.h>
+
+#include "rlImGui.h"
+#include "imgui.h"
+
 #include "map/map.hh"
+
+bool show_demo_window = true;
+
+static void draw_map(map::Map const& map)
+{
+}
+
+void draw_ui()
+{
+    rlImGuiBegin();
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+    rlImGuiEnd();
+}
 
 int main()
 {
     map::Map map;
+
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(1600, 900, "rt-map-test");
+    SetTargetFPS(60);
+
+    rlImGuiSetup(true);
+    Camera2D camera { { 0, 0 }, { 0, 0 }, 0, 1.0f };
+
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        BeginMode2D(camera);
+        draw_map(map);
+        draw_ui();
+        EndMode2D();
+
+        EndDrawing();
+
+        if (IsKeyDown(KEY_Q))
+            break;
+    }
+
+    rlImGuiEnd();
 }
