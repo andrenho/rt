1ee0e25daf017a1cfaea09de6bb9685bd38dc898
Andre Wagner <andre.nho@gmail.com>
2025-11-18 09:49:28 -0600

    .

--- common/include-common.cmake
+++ common/include-common.cmake
@@ -2,7 +2,7 @@ if (CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
     include(FetchContent)
     FetchContent_Declare(
             rt-common
-            SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../common
+            SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     )
     FetchContent_MakeAvailable(rt-common)
 endif()
