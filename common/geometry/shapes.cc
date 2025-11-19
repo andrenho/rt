8caea4743937dd3ca76ec51faba59afa093a8c69
Andre Wagner <andre.nho@gmail.com>
2025-11-18 19:58:33 -0600

    .

--- common/geometry/shapes.cc
+++ common/geometry/shapes.cc
@@ -1,1 +1,11 @@
 #include "shapes.hh"
+
+geo::Point geo::Polygon::center() const
+{
+    float x = 0.f, y = 0.f;
+    for (Point const& p: *this) {
+        x += p.x;
+        y += p.y;
+    }
+    return { x / (float) size(), y / (float) size() };
+}
--- map/test/main.cc
+++ map/test/main.cc
@@ -1,13 +1,48 @@
+#include <cstdio>
+
+#include <optional>
+
 #include <raylib.h>
+#include <raymath.h>
 
 #include "rlImGui.h"
 #include "imgui.h"
 
 #include "map/map.hh"
+#include "geometry/shapes.hh"
 
-bool show_demo_window = true;
+static bool show_demo_window = true;
+static Camera2D camera { { 0, 0 }, { 0, 0 }, 0, 1.0f };
+static map::Map map_;
 
-static void draw_map(map::Map const& map)
+static Vector2 V(geo::Point const& p) { return { p.x, p.y }; }
+
+static void draw_shape(geo::Shape const& shape, std::optional<Color> line_color={}, std::optional<Color> bg_color={})
+{
+    std::visit(overloaded {
+            [&](geo::Polygon const& p) {
+                if (bg_color) {
+                    for (size_t i = 0; i < p.size() - 1; i++)
+                        DrawTriangle(V(p.at(i)), V(p.at(i+1)), V(p.center()), *bg_color);
+                    DrawTriangle(V(p.at(p.size() - 1)), V(p.at(0)), V(p.center()), *bg_color);
+                }
+                if (line_color) {
+                    for (size_t i = 0; i < p.size(); i++) {
+                        auto a = p[i], b = p[(i + 1) % p.size()];
+                        DrawLineEx({ a.x, a.y }, { b.x, b.y }, 1.f, *line_color);
+                    }
+                }
+            },
+            [&](geo::Circle const& c) {
+                if (bg_color)
+                    DrawCircle((int) c.center.x, (int) c.center.y, c.radius, *bg_color);
+                if (line_color)
+                    DrawCircleLines((int) c.center.x, (int) c.center.y, c.radius, *line_color);
+            },
+    }, shape);
+}
+
+static void draw_map()
 {
 }
 
@@ -19,31 +54,54 @@ void draw_ui()
     rlImGuiEnd();
 }
 
+static void handle_events()
+{
+    if (IsKeyDown(KEY_Q))
+        exit(EXIT_SUCCESS);
+
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+    {
+        Vector2 delta = GetMouseDelta();
+        delta = Vector2Scale(delta, -1.0f/camera.zoom);
+        camera.target = Vector2Add(camera.target, delta);
+    }
+
+    float wheel = GetMouseWheelMove();
+    if (wheel != 0.f)
+    {
+        Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+        camera.offset = GetMousePosition();
+        camera.target = mouseWorldPos;
+
+        float scale = 0.2f*wheel;
+        camera.zoom = Clamp(expf(logf(camera.zoom)+scale), 0.125f / (256 * 256), 64.0f);
+    }
+}
+
 int main()
 {
-    map::Map map;
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(1600, 900, "rt-map-test");
     SetTargetFPS(60);
 
     rlImGuiSetup(true);
-    Camera2D camera { { 0, 0 }, { 0, 0 }, 0, 1.0f };
 
     while (!WindowShouldClose()) {
+
+        handle_events();
 
         BeginDrawing();
         ClearBackground(WHITE);
 
         BeginMode2D(camera);
-        draw_map(map);
-        draw_ui();
+        draw_shape(geo::Circle({ 100, 100 }, 50.f), BLACK, YELLOW);
+        draw_map();
         EndMode2D();
 
-        EndDrawing();
+        draw_ui();
 
-        if (IsKeyDown(KEY_Q))
-            break;
+        EndDrawing();
     }
 
     rlImGuiEnd();
