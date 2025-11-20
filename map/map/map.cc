941978fc5826c719631e2f31460c046c80f1043c
Andre Wagner <andre.nho@gmail.com>
2025-11-19 20:21:09 -0600

    .

--- map/map/map.cc
+++ map/map/map.cc
@@ -100,7 +100,10 @@ void Map::generate_polygon_heights()
 
     for (auto const& pp: polygons) {
         auto p = pp.center();
-        polygon_heights.emplace_back(perlin.noise2D_01(p.x / (float) cfg_.map_w * 2, p.y / (float) cfg_.map_h * 2));
+        float w = (float) cfg_.map_w;
+        float h = (float) cfg_.map_h;
+        float distance_from_center = ((p.x-w*0.5f)*(p.x-w*0.5f)+(p.y-h*0.5f)*(p.y-h*0.5f))/((w*0.5f)*(w*0.5f)+(h*0.5f)*(h*0.5f)) / .6f;
+        polygon_heights.emplace_back(std::clamp(1.f - (float) perlin.noise2D_01(p.x / (float) cfg_.map_w * 2, p.y / (float) cfg_.map_h * 2) * distance_from_center / .5f, .0f, 1.f));
     }
 }
 
